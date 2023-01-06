4635bab98d10aec4c93af606787b4b697cc77e59
Livya Kendi Kiruja <livyakendikiruja@gmail.com>
2023-01-06 21:23:37 +0300

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop("Ralph");
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift("Bob");
+}
+
+function appendCat(name) {
+    const copyCats = cats.slice();
+    copyCats.push(name);
+    return copyCats;
+}
+
+function prependCat(name) {
+    const copyCats = [...cats];
+    copyCats.unshift(name);
+    return copyCats;
+}
+
+function removeLastCat() {
+    const copyOfCats = [...cats];
+    copyOfCats.pop();
+    return copyOfCats;
+}
+
+function removeFirstCat() {
+    const copyOfCats = [...cats];
+    copyOfCats.shift();
+    return copyOfCats;
+}
